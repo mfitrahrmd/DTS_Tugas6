77f977fe9dafa4df5abc43093ec4be1a1ab7f014
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-11 12:28:41 +0700

    implement FindMantContainsName

--- Repositories/mssql/UniversityRepository.cs
+++ Repositories/mssql/UniversityRepository.cs
@@ -4,9 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DTS_Tugas6.Repositories.mssql;
 
-public class UniversityRepository : EFCoreRepository<int, University, DatabaseContext>
+public class UniversityRepository : EFCoreRepository<int, University, DatabaseContext>, IUniversityRepository
 {
     public UniversityRepository(DatabaseContext context) : base(context)
     {
+    }
+
+    public IEnumerable<University> FindManyContainsName(string name)
+    {
+        return _context.Set<University>().Where(u => u.Name.Contains(name));
     }
 }
