7a72c9104f580ac86d5f75b1a3dd17a38eccd635
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-12 13:32:10 +0700

    add migration

---
+++ Migrations/20230412060917_FixProfilingPK.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DTSTugas6.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixProfilingPK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_tb_m_educations_tb_m_universities_university_id",
+                table: "TB_M_Educations");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_tb_m_educations_tb_m_universities_university_id",
+                table: "TB_M_Educations",
+                column: "university_id",
+                principalTable: "TB_M_Universities",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_tb_m_educations_tb_m_universities_university_id",
+                table: "TB_M_Educations");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_tb_m_educations_tb_m_universities_university_id",
+                table: "TB_M_Educations",
+                column: "university_id",
+                principalTable: "TB_M_Universities",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
